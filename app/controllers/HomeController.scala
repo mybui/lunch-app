c0dffad09bbf9186526d582a380eb3e5e8f23a26
mybui <my.bui.fi@gmail.com>
2021-08-29 21:10:03 +0300

    day by day menu

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -20,16 +20,22 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def menu(duration: String, location: String/*, diet: String, allergy: String*/) = Action { implicit request: Request[AnyContent] =>
     if (location == "innopoli") {
       if (duration == "monday") {
-        /*
-        Ok(views.html.menu(getMealByDay(json = json, day = "monday")(0),
-          "Innopoli",
-          getMealByDay(json = json, day = "monday")(1), 
-          getMealByDay(json = json, day = "monday")(2)))
-         */
         Ok(views.html.menu(getMealByDay(json = json, day = "monday"), "Innopoli"))
       }
+      else if (duration == "tuesday") {
+        Ok(views.html.menu(getMealByDay(json = json, day = "tuesday"), "Innopoli"))
+      }
+      else if (duration == "wednesday") {
+        Ok(views.html.menu(getMealByDay(json = json, day = "wednesday"), "Innopoli"))
+      }
+      else if (duration == "thursday") {
+        Ok(views.html.menu(getMealByDay(json = json, day = "thursday"), "Innopoli"))
+      }
+      else if (duration == "friday") {
+        Ok(views.html.menu(getMealByDay(json = json, day = "friday"), "Innopoli"))
+      }
       else {
-        Ok(views.html.menu("n/a", "n/a"))
+        Ok(views.html.menu("n/a", "Innopoli"))
       }
     } else {
       Ok(views.html.menu("n/a", "n/a"))
@@ -37,56 +43,57 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   }
 
   def getMealByDay(json: JsValue, day: String): String = {
-    if (day == "monday") {
-      /*var output = Array.ofDim[String](3, 2)*/
-      var output: String = """
-                             |
-                             |
-                             |""".stripMargin + "                 MONDAY" + """
-                                                                              |
-                                                                              |""".stripMargin
-      var index: Int = 1
-       breakable
-       {
-         while (index <= 10) {
-           val title: String = getMealInfo(json = json, item = "title_en", dayNumber = 0, courseNumber = index)
-           val price: String = getMealInfo(json = json, item = "price", dayNumber = 0, courseNumber = index)
-           val diet: String = getMealInfo(json = json, item = "dietcodes", dayNumber = 0, courseNumber = index)
-           val allergy: String = getMealInfo(json = json, item = "allergy", dayNumber = 0, courseNumber = index)
-           val course: Meal = new Meal(title = title, price = price, diet = diet, allergy = allergy)
-           /*output(2)(0) = course.toString()
-           output(2)(1) = diet
-           println(output(2)(1))*/
-           if (title == "n/a") break
-           output += "                    - " + course +
-             """
-               |
-               |
-               |""".stripMargin
-           /*output :+ course.toString()*/
-           index += 1
-         }
-       }
-      /*return Array(monday.toString(), diet.replace("\"", ""), allergy.replace("\"", ""))*/
-      return output
-    } else {
-      /*val monday: Meal = new Meal(title = "n/(a)", price = "n/a", diet = "n/a", allergy = "n/a")
-      return Array(monday.toString(), "n/a", "n/a")*/
-      return "n/a"
+    var dayNumber: Int = 0
+    if (day == "tuesday") {
+      dayNumber = 1
     }
+    if (day == "wednesday") {
+      dayNumber = 2
+    }
+    if (day == "thursday") {
+      dayNumber = 3
+    }
+    if (day == "friday") {
+      dayNumber = 4
+    }
+    println(dayNumber)
+    var output: String = """|
+                            |
+                            |""".stripMargin + "                 " + day.toUpperCase() + """
+                                                                                            |
+                                                                                            |""".stripMargin
+    breakable
+    {
+      var index: Int = 1
+      while (index <= 10) {
+        val title: String = getMealInfo(json = json, item = "title_en", dayNumber = dayNumber, courseNumber = index)
+        val price: String = getMealInfo(json = json, item = "price", dayNumber = dayNumber, courseNumber = index)
+        val diet: String = getMealInfo(json = json, item = "dietcodes", dayNumber = dayNumber, courseNumber = index)
+        val allergy: String = getMealInfo(json = json, item = "allergy", dayNumber = dayNumber, courseNumber = index)
+        val course: Meal = new Meal(title = title, price = price, diet = diet, allergy = allergy)
+        if (title == "n/a") break
+        output += "                    - " + course +
+          """
+             |
+             |
+             |""".stripMargin
+        index += 1
+      }
+    }
+
+    if (dayNumber != None) {
+      return output
+    }
+    return "n/a"
   }
 
   def getMealInfo(json: JsValue, item: String, dayNumber: Int, courseNumber: Int): String = {
     var itemVal: String = "n/a"
     try {
       if (item == "allergy") {
-        /*println(dayNumber + stepper)*/
-        /*println(Json.prettyPrint((json \ "mealdates" \ dayNumber \ "courses" \ courseNumber.toString()).get))*/
         itemVal = Json.prettyPrint((json \ "mealdates" \ dayNumber \ "courses" \ courseNumber.toString() \ "additionalDietInfo" \ "allergens").get)
-        /*println(itemVal)*/
       } else {
         itemVal = Json.prettyPrint((json \ "mealdates" \ dayNumber \ "courses" \ courseNumber.toString() \ item).get)
-        /*println(itemVal)*/
       }
     } catch {
       case unknown: Throwable => println("Unknown error")
